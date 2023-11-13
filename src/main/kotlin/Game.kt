13914eebebc383d0dada96b3b39a544704038f6d
bernardo <bernardomls2004@hotmail.com>
2023-11-13 19:25:33 +0000

    Corrigido bug das escadas

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -45,14 +45,14 @@ fun Game.doAction(action: Action?): Game {
     return when (action) {
         //The function isStopped() is used to prevent man getting a new speed while moving, preventing it from misalignment of the grid (Used in all movements).
         Action.WALK_LEFT -> if (man.pos.x==0) this
-            else if (man.speed.isZero() && !man.copy(Point(man.pos.x+CELL_WIDTH,man.pos.y)).DetectIfisInsideFloor(floor) && man.DetectIfisFloororstrair(floor,stairs)==true)
+        else if (man.speed.isZero() && !man.copy(Point(man.pos.x+CELL_WIDTH,man.pos.y)).DetectIfisInsideFloor(floor) && man.DetectIfisFloororstrair(floor,stairs)==true)
             newStateMove(Direction.LEFT, man) else this
 
         Action.WALK_RIGHT -> if (man.pos.x==MAX_X) this
-            else if(man.speed.isZero() && !man.copy(Point(man.pos.x+CELL_WIDTH,man.pos.y)).DetectIfisInsideFloor(floor) && man.DetectIfisFloororstrair(floor,stairs)==true)
-                newStateMove(Direction.RIGHT, man) else this
+        else if(man.speed.isZero() && !man.copy(Point(man.pos.x+CELL_WIDTH,man.pos.y)).DetectIfisInsideFloor(floor) && man.DetectIfisFloororstrair(floor,stairs)==true)
+            newStateMove(Direction.RIGHT, man) else this
 
-        Action.UP_STAIRS ->if(man.DetectIfisStairs(stairs) && man.speed.isZero()) newStateMove(Direction.UP, man) else this
+        Action.UP_STAIRS ->if(man.DetectIfisStairs(stairs) && man.speed.isZero() && man.copy(Point(man.pos.x,man.pos.y-CELL_HEIGHT*2)).DetectIfisStairs(stairs) ) newStateMove(Direction.UP, man) else this
 
         Action.DOWN_STAIRS ->if(man.DetectIfisStairs(stairs) && !man.DetectIfisFloor(floor) && man.speed.isZero()) newStateMove(Direction.DOWN, man) else this
 
@@ -98,7 +98,7 @@ fun Game.newStateJump(direction: Direction, man: Man): Game {
  */
 
 fun Game.stepFrame(): Game {
-    //println(man)
+    println(man)
    return when{
         (man.jumpCycle > 0 && man.Food(food)) ->
             Game(man.copy(jumpCycle = man.jumpCycle - 1).jump(), floor, stairs, eggs, man.removeFood(food))
